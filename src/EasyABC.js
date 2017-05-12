8211e91a857f874d8b295b888059d27320559dae
Srivatsa S <Srivatsa S>
2017-05-12 11:39:49 +0530

    Added functionality to all the buttons and sound to the buttons

--- src/EasyABC.js
+++ src/EasyABC.js
@@ -15,23 +15,70 @@ class EasyABC extends React.Component{
       currentTick: 0
     };
     this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
+
+  //we would like to play the sound as soon as page loads
+  componentDidMount(){
+    let letterSound = document.querySelector(`audio[data-key="letter"]`);
+    //let wordSound = document.querySelector(`audio[data-key="word"]`);
+    //console.log(letterSound);
+    if(this.state.currentPosition === 0){
+      letterSound.play();
+    }
+  }
+  componentDidUpdate(){
+    this.playSound();
+  }
+
+  playSound(){
+    let letterSound = document.querySelector(`audio[data-key="letter"]`);
+    let wordSound = document.querySelector(`audio[data-key="word"]`);
+    console.log('play sound');
+
+    if(this.state.currentTick === 0){
+      letterSound.currentTime = 0;
+      letterSound.play();
+    }else{
+      wordSound.currentTime = 0;
+      wordSound.play();
+    }
   }
 
   next (){
-    console.log('next button clicked');
-    if(this.state.currentTick < 2){
-      this.setState({currentTick: this.state.currentTick + 1});
+    //console.log('next button clicked');
+    if(this.state.currentPosition === this.state.alphabets.length - 1){
+      if(this.state.currentTick < 2){
+        this.setState({currentTick: this.state.currentTick + 1});
+      }else{
+        this.setState({currentPosition: 0, currentTick: 0});
+      }
     }else{
-      this.setState({currentPosition: this.state.currentPosition + 1, currentTick: 0})
+      if(this.state.currentTick < 2){
+        this.setState({currentTick: this.state.currentTick + 1});
+      }else{
+        this.setState({currentPosition: this.state.currentPosition + 1, currentTick: 0})
+      }
+    }
+    //this.playSound();
+  }
+
+  prev(){
+    //console.log('prev button clicked');
+    if(this.state.currentPosition > 0){
+      this.setState({currentPosition: this.state.currentPosition - 1});
+    }else{
+      this.setState({currentPosition: this.state.alphabets.length - 1});
     }
   }
 
   render (){
     let showImage = this.state.currentTick !== 0 ? true : false;
-    console.log(this.state.currentTick, showImage);
+    //console.log(this.state.currentTick, showImage);
 
     let showWord = this.state.currentTick === 2 ? true : false;
-    console.log(this.state.currentTick, showWord);
+    //console.log(this.state.currentTick, showWord);
     //console.log(alphabets);
     return(
       <div className="game">
@@ -40,10 +87,14 @@ class EasyABC extends React.Component{
             <div className="field-block">
               {this.state.alphabets[this.state.currentPosition].letter}
             </div>
+            <audio src={this.state.alphabets[this.state.currentPosition].letterSound}
+              data-key="letter" />
           </div>
+          Current Position: {this.state.currentPosition}<br/>
+          Current Tick: {this.state.currentTick}
           <div className="buttons">
-            <a className="button prev">Previous</a>
-            <a className="button sound">Play Sound Again</a>
+            <a className="button prev" onClick={this.prev}>Previous</a>
+            <a className="button sound" onClick={this.playSound}>Play Sound Again</a>
             <a className="button next" onClick={this.next}>Next</a>
           </div>
           <div className="fields">
@@ -53,6 +104,8 @@ class EasyABC extends React.Component{
                 <img className = {classNames('letter-image', {hide: !showImage})}
                   alt={this.state.alphabets[this.state.currentPosition].word}
                   src={this.state.alphabets[this.state.currentPosition].image} />
+                <audio src={this.state.alphabets[this.state.currentPosition].wordSound}
+                    data-key="word" />
               </div>
               <div className="right-field">
                 <div className={classNames('placeholder-span', {hide: showWord})}>Click Next to View the Alphabet</div>
