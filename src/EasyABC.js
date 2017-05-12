6ea2e7dc287d1274d328a23dd4ee13a364abe305
Srivatsa S <Srivatsa S>
2017-05-12 10:08:39 +0530

    Adding functionality to the NEXT button

--- src/EasyABC.js
+++ src/EasyABC.js
@@ -1,5 +1,6 @@
 import React ,{ Component } from 'react';
 import alphabets from './alphabets.json';
+import classNames from 'classnames';
 
 
 class EasyABC extends React.Component{
@@ -9,11 +10,28 @@ class EasyABC extends React.Component{
     this.state = {
       alphabets: alphabets,
       //a currentposition to determine the alphabet
-      currentPosition: 7
+      currentPosition: 0,
+      //currentTick as another counter
+      currentTick: 0
     };
+    this.next = this.next.bind(this);
+  }
+
+  next (){
+    console.log('next button clicked');
+    if(this.state.currentTick < 2){
+      this.setState({currentTick: this.state.currentTick + 1});
+    }else{
+      this.setState({currentPosition: this.state.currentPosition + 1, currentTick: 0})
+    }
   }
 
   render (){
+    let showImage = this.state.currentTick !== 0 ? true : false;
+    console.log(this.state.currentTick, showImage);
+
+    let showWord = this.state.currentTick === 2 ? true : false;
+    console.log(this.state.currentTick, showWord);
     //console.log(alphabets);
     return(
       <div className="game">
@@ -26,19 +44,19 @@ class EasyABC extends React.Component{
           <div className="buttons">
             <a className="button prev">Previous</a>
             <a className="button sound">Play Sound Again</a>
-            <a className="button next">Next</a>
+            <a className="button next" onClick={this.next}>Next</a>
           </div>
           <div className="fields">
             <div className="field-block">
               <div className="left-field">
-                <div className="placeholder-span hide">Click Next to View the Image</div>
-                <img className = "letter-image"
+                <div className={classNames('placeholder-span', {hide: showImage})}>Click Next to View the Image</div>
+                <img className = {classNames('letter-image', {hide: !showImage})}
                   alt={this.state.alphabets[this.state.currentPosition].word}
                   src={this.state.alphabets[this.state.currentPosition].image} />
               </div>
               <div className="right-field">
-                <div className="placeholder-span">Click Next to View the Alphabet</div>
-                <div className="word">
+                <div className={classNames('placeholder-span', {hide: showWord})}>Click Next to View the Alphabet</div>
+                <div className={classNames('word', {hide: !showWord})}>
                   {this.state.alphabets[this.state.currentPosition].word.toUpperCase()}
                 </div>
               </div>
